11f06972053536fa2bf3e38e9e17b6f18b069fc3
Jason Tan Heng Kern <hktanadvo@gmail.com>
2024-09-04 16:02:02 +0800

    Remove console

--- src/utils/languageDetection.js
+++ src/utils/languageDetection.js
@@ -1,7 +1,6 @@
 // src/utils/languageDetection.js
 export function detectLanguage() {
 	const language = navigator.language || navigator.userLanguage;
-	console.log('Detected language:', language); // Debug: log the detected language
 
 	if (language.startsWith('zh')) {
 		return 'zh'; // Mandarin
